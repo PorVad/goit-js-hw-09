33bb90bb7103584504138ede84f189eba7c05e78
PorVad <112697838+PorVad@users.noreply.github.com>
2025-09-20 21:41:59 +0000

    Deploying to gh-pages from @ PorVad/goit-js-hw-09@616dc69296e8e41f66f3355639db526740d7af1d 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,8 +1,12 @@
-import"./assets/styles-DD3qoKza.js";import{S as e}from"./assets/vendor-CgTBfC_f.js";const i=[{preview:"https://placekitten.com/400/250",original:"https://placekitten.com/1200/800",description:"Cute kitten 1"},{preview:"https://placekitten.com/401/250",original:"https://placekitten.com/1201/800",description:"Cute kitten 2"},{preview:"https://placekitten.com/402/250",original:"https://placekitten.com/1202/800",description:"Cute kitten 3"}],a=document.querySelector(".gallery"),l=i.map(t=>`
-  <li class="gallery-item">
-    <a class="gallery-link" href="${t.original}">
-      <img class="gallery-image" src="${t.preview}" alt="${t.description}" />
-    </a>
-  </li>
-`).join("");a.innerHTML=l;new e(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});
+import"./assets/styles-Dq1NnqdG.js";import{S as t}from"./assets/vendor-CgTBfC_f.js";const i=document.querySelector(".gallery");function o(a){return a.map(({preview:l,original:e,description:r})=>`
+    <li class="gallery-item">
+      <a class="gallery-link" href="${e}">
+        <img
+          class="gallery-image"
+          src="${l}"
+          alt="${r}"
+        />
+      </a>
+    </li>
+  `).join("")}i.innerHTML=o(galleryItems);new t(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});
 //# sourceMappingURL=1-gallery.js.map
